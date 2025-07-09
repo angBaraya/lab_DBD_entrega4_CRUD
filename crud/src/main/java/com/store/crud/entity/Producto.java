8a2aab0e157ed45a77b22af36b4e60e414d31fa6
Nicolas Rojas <nicolas.rojas.p@usach.cl>
2025-07-09 01:37:46 -0400

    modifique producto

--- crud/src/main/java/com/store/crud/entity/Producto.java
+++ crud/src/main/java/com/store/crud/entity/Producto.java
@@ -15,14 +15,31 @@ public class Producto {
     @Column(name = "id_producto")
     private Long id;
 
+    @Column(name = "url", nullable = false)
     private String url;
 
+    @Column(name = "stock")
     private Integer stock;
 
+    @Column(name = "descripcion", nullable = false)
     private String descripcion;
 
+    @Column(name = "ventas")
     private Integer cantidad_ventas;
 
+    @Column(name = "precio", nullable = false)
     private Integer precio;
 
+    public Producto(Long id, String url, Integer stock, String descripcion, Integer cantidad_ventas, Integer precio) {
+        this.id = id;
+        this.url = url;
+        this.stock = stock;
+        this.descripcion = descripcion;
+        this.cantidad_ventas = cantidad_ventas;
+        this.precio = precio;
+    }
+
+    public Producto() {
+
+    }
 }
