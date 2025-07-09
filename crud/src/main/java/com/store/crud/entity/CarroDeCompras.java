e077f75e82e15e856e1fdcb652f33bba8a1ded09
Nicolas <nicolas.valdes.s@usach.cl>
2025-07-08 20:56:07 -0400

    Cambios menores

--- crud/src/main/java/com/store/crud/entity/CarroDeCompras.java
+++ crud/src/main/java/com/store/crud/entity/CarroDeCompras.java
@@ -1,4 +1,22 @@
 package com.store.crud.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+
+@Data
+@Entity
+@Table(name = "carro_de_compras")
 public class CarroDeCompras {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_carro;
+
+
+
+    
+
 }
